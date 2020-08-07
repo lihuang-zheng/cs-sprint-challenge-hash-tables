a49d0226d50010189cc8931cedc77d20b46716f4
Lihuang Zheng <jerryzheng0131@gmail.com>
2020-08-07 11:41:54 -0500

    completed ex3.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,15 +3,24 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    # store number that's repeated
+    repeated = dict()
 
-    return result
+    for array in arrays:
+        for num in array:
+            if num in repeated:
+                repeated[num] += 1
+            else:
+                repeated[num] = 1
+
+    # print(repeated)
+    # return number that have repeated multiple times in the length of the arrays.
+    return [number[0] for number in repeated.items() if number[1] == len(arrays)]
 
 
 if __name__ == "__main__":
-    arrays = []
+    arrays_test = [list(range(1000000, 2000000)) + [1, 2, 3],
+                   list(range(2000000, 3000000)) + [1, 2, 3],
+                   list(range(3000000, 4000000)) + [1, 2, 3]]
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
-
-    print(intersection(arrays))
+    print(intersection(arrays_test))
