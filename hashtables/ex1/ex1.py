f7957ecc7f1ba3a0b074774cd647f8c23b6deac4
Lihuang Zheng <jerryzheng0131@gmail.com>
2020-08-07 10:28:16 -0500

    completed ex1.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # create dictionary
+    weight_dict = dict()
 
+    # add weight to the dict
+    for item in range(len(weights)):
+        weight_dict[weights[item]] = item
+
+    # subtract each item weight from the limit weight,
+    # if the remainder is a weight in the list, return both of it with remainder
+    for item in range(len(weights)):
+        remainder = limit - weights[item]
+        if remainder in weight_dict:
+            return max(item, weight_dict[remainder]), min(item, weight_dict[remainder])
     return None
