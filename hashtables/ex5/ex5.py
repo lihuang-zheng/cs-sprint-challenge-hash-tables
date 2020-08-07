630a0b24e27a57227cdc601e7973d68a708f2fe1
Lihuang Zheng <jerryzheng0131@gmail.com>
2020-08-07 12:39:51 -0500

    completed ex5. stretched.

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,25 +1,47 @@
-# Your code here
-
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # created result array & directory dictionary
+    result = []
+    directory = dict()
+
+    # loop through files to get each files
+    for file in files:
+        # split the file with "/"
+        split_section = file.split("/")
+
+        # then take the last item from the file
+        filename = split_section[-1]
+
+        # if the file name is not in the directory we will create it with [] to include directory location.
+        if filename not in directory:
+            directory[filename] = []
+
+        # then we add the filename to the directory dictionary.
+        directory[filename].append(file)
+
+    # print(directory)
+
+    # find query filename within the queries
+    for query in queries:
+        # if query filename match the directory
+        if query in directory:
+            # then we add the query to the result.
+            result.extend(directory[query])
 
     return result
 
 
 if __name__ == "__main__":
-    files = [
+    test_files = [
         '/bin/foo',
         '/bin/bar',
         '/usr/bin/baz'
     ]
-    queries = [
+    test_query = [
         "foo",
         "qux",
         "baz"
     ]
-    print(finder(files, queries))
+    print(finder(test_files, test_query))
